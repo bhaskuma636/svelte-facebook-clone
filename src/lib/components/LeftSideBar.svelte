b3d6ef8c9b272e862fe4b5a40550a26a298c3c55
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-18 19:37:50 +0530

    🚪left side bar - nav links - hides in mobile layout

---
+++ src/lib/components/LeftSideBar.svelte
@@ -0,0 +1,54 @@
+<ul class="mt-5 flex flex-col space-y-2 w-[87%] hidden lg:flex">
+	<li class="cursor-pointer flex items-center justify-start space-x-2 bg-[#242526] rounded-lg py-1.5">
+		<div class="w-8 h-8 rounded-full ml-1">
+			<img class="w-full h-full block object-cover rounded-full shadow" src="https://scontent-maa2-1.xx.fbcdn.net/v/t1.6435-1/p160x160/140410108_522430372055176_3800608230781856331_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=7206a8&_nc_ohc=XyFyPKOtp5AAX9GSm3b&_nc_ht=scontent-maa2-1.xx&oh=be3c3cac0f69dbb82b730a1c0bb03396&oe=61BB6A90" alt="pic">
+		</div>
+		<p class="font-medium text-[0.96rem]">Manohar Maharshi</p>
+	</li>
+	<li class="cursor-pointer flex items-center justify-start space-x-2 hover:bg-[#3a3b3c] rounded-lg py-1.5">
+		<div class="w-7 h-7 rounded-full ml-1">
+			<img class="w-full h-full block object-cover" src="https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/S0U5ECzYUSu.png" alt="pic">
+		</div>
+		<p class="font-medium text-[0.96rem]">Friends</p>
+	</li>
+	<li class="cursor-pointer flex items-center justify-start space-x-2 hover:bg-[#3a3b3c] rounded-lg py-1.5">
+		<div class="w-7 h-7 rounded-full ml-1">
+			<img class="w-full h-full block object-cover" src="https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/PrjLkDYpYbH.png" alt="pic">
+		</div>
+		<p class="font-medium text-[0.96rem]">Groups</p>
+	</li>
+	<li class="cursor-pointer flex items-center justify-start space-x-2 hover:bg-[#3a3b3c] rounded-lg py-1.5">
+		<div class="w-7 h-7 rounded-full ml-1">
+			<img class="w-full h-full block object-cover" src="https://static.xx.fbcdn.net/rsrc.php/v3/yU/r/D2y-jJ2C_hO.png" alt="pic">
+		</div>
+		<p class="font-medium text-[0.96rem]">Marketplace</p>
+	</li>
+	<li class="cursor-pointer flex items-center justify-start space-x-2 hover:bg-[#3a3b3c] rounded-lg py-1.5">
+		<div class="w-7 h-7 rounded-full ml-1">
+			<img class="w-full h-full block object-cover" src="https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/duk32h44Y31.png" alt="pic">
+		</div>
+		<p class="font-medium text-[0.96rem]">Watch</p>
+	</li>
+	<li class="cursor-pointer flex items-center justify-start space-x-2 hover:bg-[#3a3b3c] rounded-lg py-1.5">
+		<div class="w-7 h-7 rounded-full ml-1">
+			<img class="w-full h-full block object-cover" src="https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/he-BkogidIc.png" alt="pic">
+		</div>
+		<p class="font-medium text-[0.96rem]">Memories</p>
+	</li>
+	<li class="cursor-pointer flex items-center justify-start space-x-2 hover:bg-[#3a3b3c] rounded-lg py-1.5">
+		<div class="w-7 h-7 rounded-full ml-1">
+			<img class="w-full h-full block object-cover" src="https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/lVijPkTeN-r.png" alt="pic">
+		</div>
+		<p class="font-medium text-[0.96rem]">Saved</p>
+	</li>
+	<li class="cursor-pointer flex items-center justify-start space-x-2 hover:bg-[#3a3b3c] rounded-lg py-1.5">
+		<div class="w-7 h-7 rounded-full ml-1">
+			<img class="w-full h-full block object-cover" src="https://static.xx.fbcdn.net/rsrc.php/v3/yH/r/kyCAf2jbZvF.png" alt="pic">
+		</div>
+		<p class="font-medium text-[0.96rem]">Pages</p>
+	</li>
+	<!-- <div class="w-full h-0.5 bg-gray-700"></div> -->
+	<li class="text-xs leading-relaxed text-gray-500 py-1 w-[80%]">
+		Privacy  · Terms  · Advertising  · Ad choices   · Cookies  · Maharshi © 2021 · Made With Svelte(kit)
+	</li>
+</ul>
