d28f230e79e378486cbac04ca80cc14e2b3e6083
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-18 19:39:05 +0530

    🗃 posts container & post card layout - responsive

---
+++ src/lib/components/Post.svelte
@@ -0,0 +1,50 @@
+<div class="py-3 pb-0 bg-[#242526] rounded-md mt-4 container mx-auto max-w-lg">
+	<div class="flex flex-col space-y-2">
+		<div class="px-4  flex items-start space-x-2 justify-between">
+			<div class="w-10 rounded-full">
+				<img class=" block rounded-full object-cover" src="https://scontent.fhyd9-1.fna.fbcdn.net/v/t1.6435-1/cp0/p40x40/188576511_333840658310001_404095601187225533_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=c6021c&_nc_ohc=yjA8PROzOZ8AX-1l_pc&_nc_ht=scontent.fhyd9-1.fna&oh=fe815f86234758dfb3e3b92e47a20aa4&oe=61BCACAF" alt="pic">
+			</div>
+			<div class="flex-1 flex flex-col">
+				<h3 class="line-clamp-1 text-base font-medium">WallPoster</h3>
+				<small class="flex items-center space-x-1.5">
+					<span>4 h</span>
+					<span>&middot;</span>
+					<svg viewBox="0 0 16 16" width="1em" height="1em" class="fill-[#fff]" title="Shared with Public"><title>Shared with Public</title><g fill-rule="evenodd" transform="translate(-448 -544)"><g><path d="M109.5 408.5c0 3.23-2.04 5.983-4.903 7.036l.07-.036c1.167-1 1.814-2.967 2-3.834.214-1 .303-1.3-.5-1.96-.31-.253-.677-.196-1.04-.476-.246-.19-.356-.59-.606-.73-.594-.337-1.107.11-1.954.223a2.666 2.666 0 0 1-1.15-.123c-.007 0-.007 0-.013-.004l-.083-.03c-.164-.082-.077-.206.006-.36h-.006c.086-.17.086-.376-.05-.529-.19-.214-.54-.214-.804-.224-.106-.003-.21 0-.313.004l-.003-.004c-.04 0-.084.004-.124.004h-.037c-.323.007-.666-.034-.893-.314-.263-.353-.29-.733.097-1.09.28-.26.863-.8 1.807-.22.603.37 1.166.667 1.666.5.33-.11.48-.303.094-.87a1.128 1.128 0 0 1-.214-.73c.067-.776.687-.84 1.164-1.2.466-.356.68-.943.546-1.457-.106-.413-.51-.873-1.28-1.01a7.49 7.49 0 0 1 6.524 7.434" transform="translate(354 143.5)"></path><path d="M104.107 415.696A7.498 7.498 0 0 1 94.5 408.5a7.48 7.48 0 0 1 3.407-6.283 5.474 5.474 0 0 0-1.653 2.334c-.753 2.217-.217 4.075 2.29 4.075.833 0 1.4.561 1.333 2.375-.013.403.52 1.78 2.45 1.89.7.04 1.184 1.053 1.33 1.74.06.29.127.65.257.97a.174.174 0 0 0 .193.096" transform="translate(354 143.5)"></path><path fill-rule="nonzero" d="M110 408.5a8 8 0 1 1-16 0 8 8 0 0 1 16 0zm-1 0a7 7 0 1 0-14 0 7 7 0 0 0 14 0z" transform="translate(354 143.5)"></path></g></g></svg> 
+				</small>		
+			</div>
+			<div>
+				<svg viewBox="0 0 20 20" width="1em" height="1em" class="fill-[#fff]"><g fill-rule="evenodd" transform="translate(-446 -350)"><path d="M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0"></path></g></svg>
+			</div>
+		</div>
+		<div>
+			<p class="px-4  line-clamp-5 mb-2">Trailer ke ila aithe repu theatre lo emavtundo😂, Jai balayya ❤️</p>
+			<img src="https://scontent.fhyd9-1.fna.fbcdn.net/v/t39.30808-6/p526x296/241351919_4465524170190502_833195494327524980_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=RSp833iNucQAX9HY6Pt&_nc_ht=scontent.fhyd9-1.fna&oh=ddcd898a0de653feb816958f65cbc50d&oe=619B11ED" alt="imf">
+		</div>
+		<div class="px-3 py-1">
+			<div class="flex items-center justify-between text-[#b0b3b8] text-sm mb-2">
+				<div class="flex items-center space-x-1">
+					<img src="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 16 16'%3e%3cdefs%3e%3clinearGradient id='a' x1='50%25' x2='50%25' y1='0%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%2318AFFF'/%3e%3cstop offset='100%25' stop-color='%230062DF'/%3e%3c/linearGradient%3e%3cfilter id='c' width='118.8%25' height='118.8%25' x='-9.4%25' y='-9.4%25' filterUnits='objectBoundingBox'%3e%3cfeGaussianBlur in='SourceAlpha' result='shadowBlurInner1' stdDeviation='1'/%3e%3cfeOffset dy='-1' in='shadowBlurInner1' result='shadowOffsetInner1'/%3e%3cfeComposite in='shadowOffsetInner1' in2='SourceAlpha' k2='-1' k3='1' operator='arithmetic' result='shadowInnerInner1'/%3e%3cfeColorMatrix in='shadowInnerInner1' values='0 0 0 0 0 0 0 0 0 0.299356041 0 0 0 0 0.681187726 0 0 0 0.3495684 0'/%3e%3c/filter%3e%3cpath id='b' d='M8 0a8 8 0 00-8 8 8 8 0 1016 0 8 8 0 00-8-8z'/%3e%3c/defs%3e%3cg fill='none'%3e%3cuse fill='url(%23a)' xlink:href='%23b'/%3e%3cuse fill='black' filter='url(%23c)' xlink:href='%23b'/%3e%3cpath fill='white' d='M12.162 7.338c.176.123.338.245.338.674 0 .43-.229.604-.474.725a.73.73 0 01.089.546c-.077.344-.392.611-.672.69.121.194.159.385.015.62-.185.295-.346.407-1.058.407H7.5c-.988 0-1.5-.546-1.5-1V7.665c0-1.23 1.467-2.275 1.467-3.13L7.361 3.47c-.005-.065.008-.224.058-.27.08-.079.301-.2.635-.2.218 0 .363.041.534.123.581.277.732.978.732 1.542 0 .271-.414 1.083-.47 1.364 0 0 .867-.192 1.879-.199 1.061-.006 1.749.19 1.749.842 0 .261-.219.523-.316.666zM3.6 7h.8a.6.6 0 01.6.6v3.8a.6.6 0 01-.6.6h-.8a.6.6 0 01-.6-.6V7.6a.6.6 0 01.6-.6z'/%3e%3c/g%3e%3c/svg%3e" alt="like">
+					<span>10</span>
+				</div>
+				<div>
+					9 Comments
+				</div>
+			</div>
+			<div class="w-full h-[1px] bg-gray-700"></div>
+			<div class="flex items-center justify-between mt-1">
+				<div class="w-full text-[#b0b3b8] flex items-center space-x-2 justify-center py-1.5 rounded-md px-3 font-medium hover:bg-[#3a3b3c] cursor-pointer">
+					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ai ai-ThumbsUp"><path d="M11.605 5.782l.23-2.369c.091-.952.98-1.598 1.878-1.366 1.351.35 2.3 1.605 2.3 3.044v3.035c0 .675 0 1.013.146 1.26.083.141.197.26.333.345.24.151.567.151 1.22.151h.396c1.703 0 2.554 0 3.078.39.393.293.67.722.78 1.208.146.65-.181 1.463-.836 3.087l-.326.81a3.261 3.261 0 0 0-.226 1.48c.232 2.874-2.047 5.295-4.833 5.136l-10.424-.599c-1.139-.065-1.708-.098-2.222-.553-.515-.455-.612-.924-.805-1.861a14.324 14.324 0 0 1 .055-6.037c.283-1.248 1.475-1.92 2.706-1.76 3.264.42 6.223-2.019 6.55-5.4z"/><path d="M7 11.5l-.137.457A14.983 14.983 0 0 0 7 21"/></svg>
+					<span>Like</span>
+				</div>
+				<div class="w-full text-[#b0b3b8] flex items-center space-x-2 justify-center py-1.5 rounded-md px-3 font-medium hover:bg-[#3a3b3c] cursor-pointer">
+					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ai ai-ChatBubble"><path d="M14 19c3.771 0 5.657 0 6.828-1.172C22 16.657 22 14.771 22 11c0-3.771 0-5.657-1.172-6.828C19.657 3 17.771 3 14 3h-4C6.229 3 4.343 3 3.172 4.172 2 5.343 2 7.229 2 11c0 3.771 0 5.657 1.172 6.828.653.654 1.528.943 2.828 1.07"/><path d="M14 19c-1.236 0-2.598.5-3.841 1.145-1.998 1.037-2.997 1.556-3.489 1.225-.492-.33-.399-1.355-.212-3.404L6.5 17.5"/></svg>
+					<span>Comment</span>
+				</div>
+				<div class="w-full text-[#b0b3b8] flex items-center space-x-2 justify-center py-1.5 rounded-md px-3 font-medium hover:bg-[#3a3b3c] cursor-pointer">
+					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ai ai-ArrowForwardThick"><path d="M22 11l-7-9v5C3.047 7 1.668 16.678 2 22c.502-2.685.735-7 13-7v5l7-9z"/></svg>
+					<span>Share</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/lib/components/PostContainer.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Post from '$lib/components/Post.svelte'
+</script>
+<main>
+	{#each Array(10) as _, index}
+		<Post />
+	{/each}
+</main>
