a5647c4beb6c054effb55febec719305289333de
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-18 22:23:25 +0530

    🚪Profile page - initial layout

---
+++ src/routes/profile/index.svelte
@@ -0,0 +1,25 @@
+<main class="text-[#e4e6eb] mt-[57px] w-full">
+	<div class="flex justify-center mt-[5rem] lg:-ml-10 lg:px-0 px-3">
+		<div class="max-w-xl bg-gray-600 w-full  rounded-lg py-2 px-5">
+			<div class="w-full flex items-center justify-start space-x-4 mr-10">		
+				<div class="w-[5rem] h-[5rem] border-4 border-gray-700 rounded-full">
+					<img class="w-full h-full object-cover rounded-full" src="https://scontent-maa2-1.xx.fbcdn.net/v/t1.6435-1/p160x160/140410108_522430372055176_3800608230781856331_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=7206a8&_nc_ohc=XyFyPKOtp5AAX9GSm3b&_nc_ht=scontent-maa2-1.xx&oh=be3c3cac0f69dbb82b730a1c0bb03396&oe=61BB6A90" alt="img">
+				</div>
+				<div class="flex-1">	
+					<h3 class="font-medium text-xl line-clamp-1 leading-relaxed">Manohar Maharshi</h3>
+					<p class="italic text-gray-300 text-sm">300 Friends</p>
+				</div>
+				<div class="flex hidden lg:block items-center mt-10 justify-center h-full">
+					<button class="bg-gray-800 flex items-center space-x-2 opacity-[0.7] hover:opacity-[1] text-sm px-3 py-1.5 rounded-lg">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mb-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+						</svg>
+						<span class="font-medium">Change Profile Pic</span>
+					</button>
+				</div>	
+			</div>	
+		</div>
+	</div>
+
+</main>	
