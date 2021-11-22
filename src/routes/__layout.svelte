e90ec974febb35f1802431e6e404c16d39023183
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-22 19:06:33 +0530

    👂onAuthStateChanged listener

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,39 @@
 <script>
+	import {onMount} from 'svelte'
+	import { app } from '$lib/store/firebase'
+	import { user,userProfileData } from '$lib/store/user'
+	import { onAuthStateChanged,getAuth } from "firebase/auth";
+	import { doc, onSnapshot, getFirestore } from "firebase/firestore";
+	import { goto } from '$app/navigation';
+
+
 	import "../app.css";
 	import NavBar from '$lib/components/NavBar.svelte';
+
+	let localUser = null;
+
+	onMount(()=>{
+		const auth = getAuth(app);
+		onAuthStateChanged(auth, (user) => {
+		  if (user) {
+		    localUser = user;
+		    console.log(user)
+			goto("/")
+		  } else {
+		  	localUser = null;
+		  	goto("/login")
+		  }
+		})
+	})
+
+	$: localUser ? $user = localUser : $user = null;
+
+	$: if($user){
+		const db = getFirestore(app);
+		onSnapshot(doc(db, "users", $user?.uid), (doc) => {
+		    $userProfileData = doc.data()
+		});
+	}
 </script>
 
 <NavBar />
