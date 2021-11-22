6d8df8566e83355da63b7d34483f83cc690beff3
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-22 19:06:10 +0530

    👂onAuthStateChanged listener

--- src/routes/login/__layout.reset.svelte
+++ src/routes/login/__layout.reset.svelte
@@ -1,1 +1,25 @@
+<script>
+	import {onMount} from 'svelte'
+	import { app } from '$lib/store/firebase'
+	import { user } from '$lib/store/user'
+	import { onAuthStateChanged,getAuth } from "firebase/auth";
+	import { goto } from '$app/navigation';
+
+	let localUser = null;
+
+	onMount(()=>{
+		const auth = getAuth(app);
+		onAuthStateChanged(auth, (user) => {
+		  if (user) {
+		    localUser = user;
+			goto("/")
+		  } else {
+		  	localUser = null;
+		  	goto("/login")
+		  }
+		})
+	})
+
+	$: localUser ? $user = localUser : $user = null;
+</script>
 <slot></slot>
