fd2b615e9439638bc2f6b760797cce36f381e7c8
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-22 19:10:15 +0530

    📍container for posts array

--- src/lib/components/PostContainer.svelte
+++ src/lib/components/PostContainer.svelte
@@ -1,8 +1,24 @@
 <script>
 	import Post from '$lib/components/Post.svelte'
+	import { app } from '$lib/store/firebase'
+	import { user } from '$lib/store/user'
+	import { doc, setDoc,deleteDoc  ,getFirestore,getDoc,query, orderBy  } from "firebase/firestore";
+	import { collection,onSnapshot,serverTimestamp } from "firebase/firestore";
+
+	let userPosts = [];
+
+	$: if($user){
+		const db = getFirestore(app);
+		onSnapshot(query(collection(db, "users", $user?.uid,"posts"),orderBy("uploadTime", "asc")), (querySnapshot) => {
+			userPosts = [];
+			querySnapshot.forEach(async(docOne) => {
+				userPosts = [docOne.data(),...userPosts]
+			});
+		});
+	}
 </script>
 <main>
-	{#each Array(1) as _, index}
-		<Post />
+	{#each userPosts as item, index}
+		<Post post={item} />
 	{/each}
 </main>
