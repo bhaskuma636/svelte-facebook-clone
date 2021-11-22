a3d42429472ca0e043602e3d9a699e6588b243b3
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-22 19:05:45 +0530

    🖇handleLogIn and handleSignUp - @firebase auth

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,9 +1,105 @@
 <script>
+	import generateName from 'sillyname';
+	import generate from 'ricknmortyquotes'
+	import { app } from '$lib/store/firebase'
+	import { user } from '$lib/store/user'
+	import { goto } from '$app/navigation';
+	import { getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+	import { getFirestore } from "firebase/firestore";
+	import { doc, setDoc, serverTimestamp  } from "firebase/firestore"; 
+
+
 	let isSignUpFormOpen = false;
-	let error = false;
+	let errorMsg = "";
+	let isLoading = false;
+
+	let loginEmail = "";
+	let loginPassword = "";
+
+	let signUpEmail = "";
+	let signUpPasswrod = "";
+	let signUpConfirmPasswrod = "";
+
+
+	const handleLogin = async () => {
+		isLoading = true;
+		errorMsg = "";
+		if (loginEmail.trim() != "" && loginPassword.trim() != "") {
+			const auth = getAuth(app);
+	    	signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+			  .then((userCredential) => {
+			    $user = userCredential.user;
+			    goto("/")
+			    isLoading = false;
+			  })
+			  .catch((error) => {
+				console.log(error)
+			  	errorMsg = error?.message
+			  	isLoading = false;
+			  }).finally(() => {
+			  	isLoading = false;
+			  })
+		}else{
+			isLoading = false;
+			errorMsg = "";
+			return;
+		}
+	}
+
+	const handleSignUp = async () => {
+		isLoading = true;
+		errorMsg = "";
+		if (signUpEmail.trim() != "" && signUpPasswrod.trim() != "" && signUpConfirmPasswrod.trim() != "") {
+			if (signUpPasswrod === signUpConfirmPasswrod) {
+				const auth = getAuth(app);
+				createUserWithEmailAndPassword(auth, signUpEmail, signUpPasswrod)
+				  .then(async (userCredential) => {
+				    $user = userCredential.user;
+				    if($user){
+				    	const db = getFirestore(app);
+						await setDoc(doc(db, "users", $user?.uid), {
+						  name: $user?.displayName || generateName(),
+						  email: $user?.email,
+						  photo: $user?.photoURL || `https://avatars.dicebear.com/api/adventurer/${$user?.uid}.svg`,
+						  createdAt : serverTimestamp(),
+						  bio : generate(),
+						});
+						await setDoc(doc(db, "users", $user?.uid), {
+						  uid : $user?.uid,
+						},{ merge: true });
+						errorMsg = "Account Created Successfully !!!"
+						goto("/")
+				    }
+				    isLoading = false;
+				  })
+				  .catch((error) => {
+				  	console.log(error)
+				  	errorMsg = error;
+				  	isLoading = false;
+				  }).finally(() => {
+				  	isLoading = false;
+				  })
+			}else{
+				errorMsg = "Passwords didn't Match"
+				isLoading = false;
+				errorMsg = "";
+			}
+		}else{
+			isLoading = false;
+			errorMsg = "";
+			return;
+		}
+	}
+
+	$: if (errorMsg) {
+		setTimeout(() => {
+			errorMsg = "";
+		},4000)
+	}
+
 </script>
-<main class="w-full h-screen bg-[#f0f2f5]">
-	<div class="w-full h-full container mx-auto md:max-w-screen-lg md:flex items-center justify-center md:space-x-10 pt-7 md:pb-[5rem]">
+<main class="w-full h-screen bg-[#f0f2f5] text-gray-900">
+	<div class="w-full px-4 sm:px-6 md:px-0 h-full container mx-auto md:max-w-screen-lg md:flex items-center justify-center md:space-x-10 pt-7 md:pb-[5rem]">
 		<div class="flex-1 container mx-auto text-center md:text-left w-full md:px-0 px-10 md:pt-0 py-4 pt-0">
 			<div class="flex flex-col items-start w-full">
 				<p class="text-[4rem] w-full text-[#1877f2] font-bold leading-tight select-none">facebook</p>
@@ -11,26 +107,26 @@
 			</div>
 		</div>
 		<div class="md:w-[40%] w-full shadow-xl border border-gray-300 py-9 p-5 rounded-lg flex flex-col space-y-6">
-			{#if error}			
-				<div class="w-full bg-red-500 -my-2 text-white px-2 py-2 rounded-lg line-clamp-1 overflow-hidden">	
-					<p class="text-sm">user not found</p>
+			{#if errorMsg}			
+				<div class="w-full bg-red-500 -my-2 text-white px-2 py-2 rounded-lg">	
+					<p class="text-sm">{errorMsg}</p>
 				</div>	
 			{/if}
 			{#if !isSignUpFormOpen}	
-				<form class="flex items-center flex-col space-y-5 w-full">
-					<input required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2]" type="email" placeholder="Email address">
-					<input required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2]" type="password" placeholder="Password">
+				<form on:submit|preventDefault={handleLogin} class="flex items-center flex-col space-y-5 w-full">
+					<input bind:value={loginEmail} required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2] text-gray-900" type="email" placeholder="Email address">
+					<input bind:value={loginPassword} required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2] text-gray-900" type="password" placeholder="Password">
 					<div class="pt-4 w-full">
-					    <button class="bg-[#1877f2] hover:bg-blue-600 active:transform active:scale-[0.99] w-full py-2.5 text-xl text-white font-bold rounded-lg">Log In</button>
+					    <button disabled={isLoading} type="submit" class="bg-[#1877f2] hover:bg-blue-600 active:transform active:scale-[0.99] w-full py-2.5 text-xl text-white font-bold rounded-lg">{ isLoading ? 'Authenticating...' : 'Log In' }</button>
 					</div>
 				</form>
 		    {:else}
-				<form class="flex items-center flex-col space-y-5 w-full">
-					<input required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2]" type="email" placeholder="Email address">
-					<input required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2]" type="password" placeholder="Password">
-					<input required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2]" type="password" placeholder="Confirm Password">
+				<form on:submit|preventDefault={handleSignUp} class="flex items-center flex-col space-y-5 w-full">
+					<input bind:value={signUpEmail} required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2] text-gray-900" type="email" placeholder="Email address">
+					<input bind:value={signUpPasswrod} required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2] text-gray-900" type="password" placeholder="Password">
+					<input bind:value={signUpConfirmPasswrod} required class="w-full border border-gray-300 px-3 py-2.5 rounded-lg focus:outline-none focus:ring focus:ring-[#1877f2] text-gray-900" type="password" placeholder="Confirm Password">
 					<div class="pt-4 w-full">
-					    <button class="bg-[#1877f2] w-full py-2.5 text-xl text-white font-bold rounded-lg">Sign Up</button>
+					    <button disabled={isLoading} type="submit" class="bg-[#1877f2] hover:bg-blue-600 active:transform active:scale-[0.99] w-full py-2.5 text-xl text-white font-bold rounded-lg">{ isLoading ? 'Authenticating...' : 'Sign Up' }</button>
 					</div>
 				</form>
 			{/if}
